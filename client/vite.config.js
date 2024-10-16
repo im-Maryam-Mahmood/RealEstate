6b0ebf60abb6ba0af84e8dfb07920c0c2332a2f1
im-Maryam-Mahmood <maryammahmoodpersonal@gmail.com>
2024-10-17 03:04:30 +0500

    Sign up Page Functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envPrefix: 'VITE_',
+  server: {
+    proxy:{
+      '/api': {
+        target:'http://localhost:3000',
+        secure:false,
+    },
+    },
+  },
   plugins: [react()],
-})
+});
