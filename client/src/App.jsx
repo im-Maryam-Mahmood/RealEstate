b36d4377547f4e3433f95ec42d9a2750bb665319
im-Maryam-Mahmood <maryammahmoodpersonal@gmail.com>
2024-10-17 10:12:14 +0500

    Add Redux Toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
+import SignIn from './pages/SignIn'
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
 
