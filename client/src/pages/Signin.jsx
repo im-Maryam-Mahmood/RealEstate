eb80b565acb33f6761e27a4fd25e146b71d0ad64
im-Maryam-Mahmood <maryammahmoodpersonal@gmail.com>
2024-10-17 10:17:42 +0500

    Redux Persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,7 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from '../redux/user/userSlice';
-import OAuth from '../components/OAuth';
+// import OAuth from '../components/OAuth';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -68,7 +67,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
-        <OAuth/>
+        {/* <OAuth/> */}
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
